98b553296d035a7920ca39a6c1e46ba9b2b7c2a1
Alexey Gulev <me@agulev.com>
2019-09-03 21:21:17 +0200

    Added `#include <stdlib.h>` for compatibility with clang

--- gpgs/src/gpgs_callback.cpp
+++ gpgs/src/gpgs_callback.cpp
@@ -1,6 +1,7 @@
 #if defined(DM_PLATFORM_IOS) || defined(DM_PLATFORM_ANDROID)
 #include "private_gpgs_callback.h"
 #include "utils/LuaUtils.h"
+#include <stdlib.h>
 
 static GPGS_callback            m_callback;
 static dmArray<CallbackData>    m_callbacksQueue;
